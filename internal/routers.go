11c39bc688b5d3051b0fe721a763bc2437655575
xtldrth <sergei.iudenichev@gmail.com>
2026-05-25 20:52:43 +0200

    fix routes

--- internal/routers.go
+++ internal/routers.go
@@ -4,9 +4,9 @@ import "net/http"
 
 func RegisterSubscriptionsRoutes(mux *http.ServeMux, handler SubscriptionsHandler) {
 	mux.HandleFunc("GET /api/subscriptions", handler.Get)
-	mux.HandleFunc("POST /api/subscriptions", handler.Create)
-	mux.HandleFunc("PATCH /api/subscriptions", handler.Update)
-	mux.HandleFunc("DELETE /api/subscriptions", handler.Delete)
 	mux.HandleFunc("GET /api/subscriptions/sum/", handler.GetPricesSum)
 	mux.HandleFunc("GET /api/subscriptions/{id}/", handler.GetByID)
+	mux.HandleFunc("POST /api/subscriptions", handler.Create)
+	mux.HandleFunc("DELETE /api/subscriptions/{id}", handler.Delete)
+	mux.HandleFunc("PATCH /api/subscriptions/{id}", handler.Update)
 }
